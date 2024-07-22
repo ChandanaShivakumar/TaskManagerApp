c489081f6ae6208152fa08cd40435f533cd4ca1e
ChandanaShivakumar <chandanashiva55@gmail.com>
2024-07-23 01:44:31 +0530

    Update index.js
    Access to XMLHttpRequest at 'https://task-manager-2sw69bapb-chandanashivakumars-projects.vercel.app/login?email=chang%40gmail.com&password=chan%4012345' from origin 'https://task-manager-app-frontend-h2lwswj0w.vercel.app' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
    
    
    adding cors policy

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,18 @@ app.get("/",(req,res)=>{
 app.use(express.json())
 
 //cors
-app.use(cors())
+// app.use(cors())
+const allowedOrigins = ['https://task-manager-app-frontend-h2lwswj0w.vercel.app'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 
 
 app.get("/users", (req,res)=>{
